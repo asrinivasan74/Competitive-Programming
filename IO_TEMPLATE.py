017b150f05f8beca461a6651b031f57f8791d125
asrinivasan74 <64226286+asrinivasan74@users.noreply.github.com>
2021-04-23 22:23:42 -0400

    Add files via upload
    fastio for python

---
+++ IO_TEMPLATE.py
@@ -0,0 +1,15 @@
+import sys
+
+inp = sys.stdin.readlines(); ii = 0
+
+out = []
+
+t = int(inp[ii])
+ii += 1
+
+sys.stdout.write(str())
+
+
+import io
+import os
+input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
