e90ba5fbd9c49d532a93b9e698f7127ca9b2b13d
asrinivasan74 <64226286+asrinivasan74@users.noreply.github.com>
2021-04-23 22:24:35 -0400

    Update IO_TEMPLATE.py

--- IO_TEMPLATE.py
+++ IO_TEMPLATE.py
@@ -1,15 +1,3 @@
-import sys
-
-inp = sys.stdin.readlines(); ii = 0
-
-out = []
-
-t = int(inp[ii])
-ii += 1
-
-sys.stdout.write(str())
-
-
 import io
 import os
 input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
